e3ae441c8791861500257adff96fcc61d733b80b
nuridze <113994431+nuridze@users.noreply.github.com>
2024-02-25 20:27:43 +0100

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,68 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 import axios from 'axios';
+import { createGalleryMarkup } from './render-functions.js';
+import { lightbox } from '../main.js';
 
-export async function fetchData(query, currentPage) {
-  const API_KEY = '42152818-0d69fd49112a74751654c42bc';
-  const url = 'https://pixabay.com/api/';
+const API_KEY = '42334631-07f239856d3b6a49db441bfb9';
+let totalHits = 0;
+let imagesShown = 0;
 
-  const params = {
-    key: API_KEY,
-    lang: 'en',
-    order: 'popular',
-    q: query,
-    per_page: 15,
-    page: currentPage,
-  };
-  const res = await axios.get(url, { params });
-  return res.data;
+// Функция дебаунсинга
+function debounce(func, delay) {
+    let debounceTimer;
+    return function (...args) {
+        const context = this;
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+}
+
+// Функция прокрутки
+function scrollGallery() {
+    const galleryCard = document.querySelector('.photo-card');
+    const cardHeight = galleryCard.getBoundingClientRect().height;
+    window.scrollBy({ top: cardHeight * 2, behavior: 'smooth' });
+}
+
+// Дебаунсинговая версия функции прокрутки
+const debouncedScrollGallery = debounce(scrollGallery, 100);
+
+export async function fetchImages(query, page) {
+    const loader = document.querySelector('.loader');
+    const loadMoreButton = document.querySelector('#load-more');
+    loadMoreButton.style.display = 'none'; // Скрываем кнопку Load more во время загрузки
+    loader.style.display = 'block';
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`); // Добавлены параметры per_page и page
+        totalHits = response.data.totalHits;
+        imagesShown += response.data.hits.length;
+        console.log(response.data);
+        loader.style.display = 'none';
+        if (response.data.hits.length === 0) {
+            if (totalHits === 0) {
+                iziToast.info({
+                    title: 'Info',
+                    message: 'Sorry, there are no images matching your search query. Please try again!'
+                });
+            } else if (imagesShown >= totalHits) {
+                iziToast.info({
+                    title: 'Info',
+                    message: 'We are sorry, but you have reached the end of search results.'
+                });
+            }
+        } else {
+            createGalleryMarkup(response.data.hits);
+            lightbox.refresh();
+            debouncedScrollGallery(); //Дебаунсинговая функция
+            loadMoreButton.style.display = 'block'; // Показываем кнопку Load more после загрузки
+        }
+    } catch (error) {
+        console.error('Error fetching images:', error);
+        loader.style.display = 'none';
+        iziToast.error({
+            title: 'Error',
+            message: 'Failed to fetch images. Please try again later.'
+        });
+    }
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,18 +1,28 @@
-function imgTemplate(hit) {
-    const {
-      largeImageURL,
-      webformatURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    } = hit;
-    return `<li class="list-item"><a href="${largeImageURL}">
-            <img class="item-img" src="${webformatURL}" alt="${tags}" ></a><div class="container"><p><b>Likes: </b><br>${likes}</p><p><b>Views: </b><br>${views}</p><p><b>Comments: </b><br>${comments}</p><p><b>Downloads: </b><br>${downloads}</p>
-            </div></li>`;
+import { lightbox } from '../main.js';
+
+export function createGalleryMarkup(images, append = true) {
+  const gallery = document.querySelector('.gallery');
+  // gallery.innerHTML = '';
+  const markup = images
+    .map((image) => `
+      <div class="photo-card">
+        <a href="${image.largeImageURL}" target="_blank">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p><i class="img-text">Likes </i><span>${image.likes}</span></p>
+          <p><i class="img-text">Views </i><span>${image.views}</span></p>
+          <p><i class="img-text">Comments </i><span>${image.comments}</span></p>
+          <p><i class="img-text">Downloads </i><span>${image.downloads}</span></p>
+        </div>
+      </div>
+    `)
+    .join('');
+  if (append) {
+    gallery.insertAdjacentHTML('beforeend', markup);
+  } else {
+    gallery.innerHTML = markup;
   }
-  
-  export function imgsTemplate(hits) {
-    return hits.map(imgTemplate).join('');
-  }
+
+  lightbox.refresh();
+}
